ee6563fdcf42a5e4ddb7e4464b6c778788d48a62
Lucy Hao <lucyhao@mit.edu>
2022-01-14 01:51:57 -0500

    revert to skeleton

--- server/server.js
+++ server/server.js
@@ -34,7 +34,11 @@ const socketManager = require("./server-socket");
 // Server configuration below
 // TODO change connection URL after setting up your team database
 <<<<<<< HEAD
+<<<<<<< HEAD
 const mongoConnectionURL = "mongodb+srv://miho_koda:Huangmeihui1.@cluster0.l9ybz.mongodb.net/test?retryWrites=true&w=majority";
+=======
+const mongoConnectionURL = "FILL ME IN";
+>>>>>>> parent of 95cdd8f (Skeleton code)
 =======
 const mongoConnectionURL = "FILL ME IN";
 >>>>>>> parent of 95cdd8f (Skeleton code)
