6b8f458a101e76bc78dad2086838c68fae16a212
Lucy Hao <lucyhao@mit.edu>
2022-01-14 02:06:06 -0500

    is it fixed now

--- server/server.js
+++ server/server.js
@@ -33,19 +33,7 @@ const socketManager = require("./server-socket");
 
 // Server configuration below
 // TODO change connection URL after setting up your team database
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 const mongoConnectionURL = "mongodb+srv://miho_koda:Huangmeihui1.@cluster0.l9ybz.mongodb.net/test?retryWrites=true&w=majority";
-=======
-const mongoConnectionURL = "FILL ME IN";
->>>>>>> parent of 95cdd8f (Skeleton code)
-=======
-const mongoConnectionURL = "FILL ME IN";
->>>>>>> parent of 95cdd8f (Skeleton code)
-=======
-const mongoConnectionURL = "FILL ME IN";
->>>>>>> parent of 95cdd8f (Skeleton code)
 // TODO change database name to the name you chose
 const databaseName = "FILL ME IN";
 
