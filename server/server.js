44ae81979b5718dd90d0c06e96daf9d6e564fa0e
lucyhaoo <96418904+lucyhaoo@users.noreply.github.com>
2022-01-18 17:17:33 -0500

    debug

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ const socketManager = require("./server-socket");
 
 // Server configuration below
 // TODO change connection URL after setting up your team database
-const mongoConnectionURL = "mongodb+srv://dearballons:<password>@cluster0.f0bxj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const mongoConnectionURL = "mongodb+srv://dearballons:inferno2022@cluster0.f0bxj.mongodb.net/test?retryWrites=true&w=majority";
 
 
 // TODO change database name to the name you chose
