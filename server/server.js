cc81bf39e1574e400285c693efc7c20918c49371
lucyhaoo <96418904+lucyhaoo@users.noreply.github.com>
2022-01-20 12:57:22 -0500

    adding mongo srv to .env

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ const socketManager = require("./server-socket");
 
 // Server configuration below
 // TODO change connection URL after setting up your team database
-const mongoConnectionURL = "mongodb+srv://dearballons:inferno2022@cluster0.f0bxj.mongodb.net/test?retryWrites=true&w=majority";
+const mongoConnectionURL = process.env.MONGO_SRV;
 
 
 // TODO change database name to the name you chose
