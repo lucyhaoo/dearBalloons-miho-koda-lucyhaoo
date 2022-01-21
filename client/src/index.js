3aaa17451a3560833e2c8975d4906f51180b69ac
Andrew Cai <andrewcai@C02FK95LMD6M.tld>
2022-01-21 00:18:25 -0500

    deploh

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App.js";
-import {BrowserRouter} from 'react-router-dom';
 
 // renders React Component "Root" into the DOM element with ID "root"
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>, 
+        <App />,
     document.getElementById("root"));
 
 // allows for live updating
