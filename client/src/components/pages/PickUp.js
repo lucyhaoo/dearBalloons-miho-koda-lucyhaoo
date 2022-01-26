3d03bf209f8e1f92039ba3ecdcaf52e8bb3a5116
Andrew Cai <andrewcai@C02FK95LMD6M.tld>
2022-01-26 11:26:47 -0500

    merge

--- client/src/components/pages/PickUp.js
+++ client/src/components/pages/PickUp.js
@@ -1,18 +1,15 @@
-import React, { useState } from 'react';
+import React from "react";
 import { get, post } from "../../utilities";
 import "./PickUp.css";
 import { Button, Flex, Spacer } from '@chakra-ui/react';
-import { Box } from '@chakra-ui/react';
+
 
 const PickUp = () => {
-  const [message, setMessage] = useState('');
-  const getMessage = async () => {
-    try {
-      const response = await get("/api/getmessage");
-      setMessage(response);
-    } catch (e) {
-      setMessage({content: "No message is left here :((((((", sender_mail: ""});
-    }ß
+  
+  const getMessage = (value) => {
+    get("/api/getmessage").then((newbody) => {
+      console.log(newbody.sender_mail);
+    });
   };
 
 
@@ -23,12 +20,9 @@ const PickUp = () => {
         GO!
       </Button>
         <Spacer />
-      <Button size='md' boxShadow='dark-lg' variant='light_blue'><a href="/">Back</a></Button>
 
-      <Box bg='dark_blue' w='50%' p={4}>
-        {message.content + message.sender_mail}
-      </Box>
-ß
+      <Button size='md' boxShadow='dark-lg' variant='dark_blue'><a href="/">Back</a></Button>
+
     </div>
   );
 };
